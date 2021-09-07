ec97d5e452b711d4634e7333533d40b2b7476629
smeek <smeek@tutanota.com>
2021-09-07 00:05:57 -0400

    initial commit

---
+++ Dulcet Chat/Dulcet Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dulcet Chat
+//
+//  Created by 🐱 on 9/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dulcet Chat/Dulcet Chat/Dulcet_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dulcet_ChatApp.swift
+//  Dulcet Chat
+//
+//  Created by 🐱 on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Dulcet_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
